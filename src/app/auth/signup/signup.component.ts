98390798aff3677377fcdb7abad549e0246999d9
Calista Pemberton <minervamachina@gmail.com>
2022-05-14 21:19:02 +0800

    add async validator for unique username

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { MatchPassword } from '../validators/match-password';
+import { UniqueUsername } from '../validators/unique-username';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -14,7 +16,7 @@ export class SignupComponent implements OnInit {
       Validators.minLength(3),
       Validators.maxLength(20),
       Validators.pattern(/^[a-z0-9]+$/)
-    ]),
+    ], [this.uniqueUsername.validate]),
     password: new FormControl('', [
       Validators.required,
       Validators.minLength(4),
@@ -27,7 +29,10 @@ export class SignupComponent implements OnInit {
     ]),
   }, { validators: [this.matchPassword.validate] })
 
-  constructor(private matchPassword: MatchPassword) { }
+  constructor(
+    private matchPassword: MatchPassword,
+    private uniqueUsername: UniqueUsername
+  ) { }
 
   ngOnInit(): void {
   }
